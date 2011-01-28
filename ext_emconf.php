f899edc87b3af632d449228c8ea5ebbd22de603b
chrissitopher <chrissitopher@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-28 17:28:13 +0000

    Updated em_conf for release of next version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simulatebe/trunk@42757 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "simulatebe".
 #
-# Auto generated 28-06-2010 16:14
+# Auto generated 28-01-2011 18:30
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,8 +11,8 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'BE-login simulation for fe-users',
-	'description' => 'Simulates BE-login for FE-users. With this extension you can grant frontend users backend user rights. This means that if a frontend user logs into the frontend and she has the proper rights the edit icons will be displayed allowing her to edit the content.',
+	'title' => 'BE Login Simulation for FE Users',
+	'description' => 'Simulates BE Login for FE Users. With this extension you can grant Frontend Users Backend User Rights. This means that, if a Frontend User logs into the Frontend and he has the proper rights, the Edit Icons will be displayed allowing him to edit the Content.',
 	'category' => 'fe',
 	'shy' => 0,
 	'dependencies' => 'cms',
@@ -20,10 +20,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
+	'doNotLoadInFE' => 0,
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
-	'modify_tables' => 'fe_users',
+	'modify_tables' => 'fe_users,be_users',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author' => 'Sonja Scholz',
@@ -43,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"8f1a";s:12:"ext_icon.gif";s:4:"103a";s:17:"ext_localconf.php";s:4:"1ca8";s:14:"ext_tables.php";s:4:"e9e3";s:14:"ext_tables.sql";s:4:"02ac";s:28:"ext_typoscript_constants.txt";s:4:"f265";s:24:"ext_typoscript_setup.txt";s:4:"c24f";s:16:"locallang_db.php";s:4:"5946";s:14:"doc/manual.sxw";s:4:"383d";s:19:"doc/wizard_form.dat";s:4:"ebe7";s:20:"doc/wizard_form.html";s:4:"9c48";s:31:"pi1/class.tx_simulatebe_pi1.php";s:4:"33e0";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"9acf";s:12:"ext_icon.gif";s:4:"103a";s:17:"ext_localconf.php";s:4:"1ca8";s:14:"ext_tables.php";s:4:"3366";s:14:"ext_tables.sql";s:4:"02ac";s:28:"ext_typoscript_constants.txt";s:4:"f265";s:24:"ext_typoscript_setup.txt";s:4:"c24f";s:16:"locallang_db.xml";s:4:"97c4";s:14:"doc/manual.sxw";s:4:"383d";s:19:"doc/wizard_form.dat";s:4:"ebe7";s:20:"doc/wizard_form.html";s:4:"9c48";s:31:"pi1/class.tx_simulatebe_pi1.php";s:4:"9b90";}',
 	'suggests' => array(
 	),
 );
