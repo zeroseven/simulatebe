e1c53015690bd023bff75af0229a5701ca66750b
sonne <sonne@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-04 08:40:11 +0000

    raised version to 2.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simulatebe/trunk@43069 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "simulatebe".
 #
-# Auto generated 30-01-2011 15:36
+# Auto generated 04-02-2011 09:38
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'cab services ag',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '3.5.0-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"9acf";s:11:"Licence.txt";s:4:"3555";s:12:"ext_icon.gif";s:4:"a696";s:17:"ext_localconf.php";s:4:"1ca8";s:14:"ext_tables.php";s:4:"3366";s:14:"ext_tables.sql";s:4:"02ac";s:28:"ext_typoscript_constants.txt";s:4:"f265";s:24:"ext_typoscript_setup.txt";s:4:"c24f";s:16:"locallang_db.xml";s:4:"97c4";s:14:"doc/manual.sxw";s:4:"383d";s:19:"doc/wizard_form.dat";s:4:"ebe7";s:20:"doc/wizard_form.html";s:4:"9c48";s:31:"pi1/class.tx_simulatebe_pi1.php";s:4:"9b90";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"a092";s:11:"Licence.txt";s:4:"3555";s:12:"ext_icon.gif";s:4:"a696";s:17:"ext_localconf.php";s:4:"1ca8";s:14:"ext_tables.php";s:4:"3366";s:14:"ext_tables.sql";s:4:"02ac";s:28:"ext_typoscript_constants.txt";s:4:"f265";s:24:"ext_typoscript_setup.txt";s:4:"c24f";s:16:"locallang_db.xml";s:4:"97c4";s:14:"doc/manual.sxw";s:4:"383d";s:19:"doc/wizard_form.dat";s:4:"ebe7";s:20:"doc/wizard_form.html";s:4:"9c48";s:31:"pi1/class.tx_simulatebe_pi1.php";s:4:"9b90";}',
 	'suggests' => array(
 	),
 );
