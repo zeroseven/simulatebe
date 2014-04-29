dc18a9888dc2c55eac77571122b3be230cbb7590
nils.blattner <nils.blattner@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-04-29 11:42:38 +0000

    Version 2.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/simulatebe/trunk@85014 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,12 @@ if (!defined('TYPO3_MODE')) {
 	// Add Plugin to Static Template #43
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_simulatebe_pi1.php', '_pi1', '', 0);
 
+if(!function_exists('user_belogged_in')) {
+	function user_belogged_in() {
+		if(isset($GLOBALS['BE_USER'])) {
+			return !empty($GLOBALS['BE_USER']->user['uid']);
+		};
+		return false;
+	}
+}
 ?>
