9bffc01adbf4691f60cf612083530bac814cd64a
Markus Poerschke <poerschke@cvc.digital>
2019-08-20 13:42:32 +0200

    Set minimum TYPO3 version to 9.5.9
    The patch version contains a fix, so that sending multiple cookies
    is possible now.

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'cab services ag',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.9-9.5.99',
             'php' => '7.2.0-0.0.0',
         ],
         'conflicts' => [],
